0b6933b7cf8ab2e7be831cc06b0f402ca302ac7d
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-05-19 12:53:59 -0700

    Added license

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// See https://github.com/derhuerst/bvg-rest/blob/master/docs/index.md
 /// for api info
 
